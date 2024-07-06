8721eda1b209588e20eeaa2a519fcc8aa19af3e9
Zevaldo <chillydudee@gmail.com>
2024-07-06 21:41:26 +0700

    menghapus file privacyPolicy

--- admin/user/action-delete.php
+++ admin/user/action-delete.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include '../../koneksi.php';
 $uid = $_GET["uid"];
@@ -6,13 +5,4 @@ $delete = mysqli_query($koneksi, "delete from user where uid='$uid'");
 if ($delete){
     header("location:../../admin.php");
 }
-=======
-<?php
-include '../../koneksi.php';
-$uid = $_GET["uid"];
-$delete = mysqli_query($koneksi, "delete from user where uid='$uid'");
-if ($delete){
-    header("location:../../admin.php");
-}
->>>>>>> aa9b696 (ngetest lagi)
 ?>
--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 <?php
     $koneksi = mysqli_connect("localhost","root",'',"chelsifootwear");
     if (mysqli_connect_errno()){
         echo "koneksi database gagal : " . mysqli_connect_error();
     }
-=======
-<?php
-    $koneksi = mysqli_connect("localhost","root",'',"chelsifootwear");
->>>>>>> aa9b696 (ngetest lagi)
 ?>    

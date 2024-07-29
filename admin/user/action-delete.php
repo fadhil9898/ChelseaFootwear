d0c488f3b9076346b3c7dbd417ce27a8862e8c7b
Zevaldo <chillydudee@gmail.com>
2024-07-29 09:09:04 +0700

    HAPUS SEMUA

--- admin/user/action-delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../../koneksi.php';
-$uid = $_GET["uid"];
-$delete = mysqli_query($koneksi, "delete from user where uid='$uid'");
-if ($delete){
-    header("location:../../admin.php");
-}
-?>
--- koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("localhost","root",'',"chelsifootwear");
-    if (mysqli_connect_errno()){
-        echo "koneksi database gagal : " . mysqli_connect_error();
-    }
-?>    
